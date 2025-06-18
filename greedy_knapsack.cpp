00d35b12731a4c8c22973ec063f16168f0b2d4e2
Debajyoti Das <debajyotid212@gmail.com>
2025-06-18 23:03:28 +0600

    Update greedy_knapsack.cpp

--- greedy_knapsack.cpp
+++ greedy_knapsack.cpp
@@ -1,54 +1,68 @@
+//Everything will be something that you've seen before, Don't panic.
+//For my Mother and Departed Father
+
 #include<bits/stdc++.h>
-#define N 1000
+#define fast() ios_base::sync_with_stdio(false);cin.tie(nullptr);
+#define ll long long
+#define nl '\n'
 using namespace std;
+const int N = 1e9;
 
-struct Item {
-    int value, weight;
-    double ratio;
-};
-
-bool compare(Item a, Item b) {
-    return a.ratio > b.ratio;
+bool isPrime(ll n, ll i = 2) {
+    if (n <= 1) return false;
+    if (i * i > n) return true;
+    if (n % i == 0) return false;
+    return isPrime(n, i + 1);
 }
 
-int main() {
-    int n, m; 
-    cin >> n >> m;
-    int profit[n], weight[n];
+void thirteen(){
+	int number_of_items, max_capacity; cin >> number_of_items >> max_capacity;
 
-    double x[n];
-    for(int i = 0; i < m; i++){
-        x[i] = 0.0;
-    }
-    
-    vector<Item> items(n);
+	vector<pair<int, int>> items(number_of_items);
+	vector<double> ratio(number_of_items);
+	vector<int> index(number_of_items);
+	double total_profit = 0.0;
 
-    for (int i = 0; i < n; i++) {
-        cin >> profit[i] >> weight[i];
-        items[i].value = profit[i];
-        items[i].weight = weight[i];
-        items[i].ratio = (double)profit[i] / weight[i];
-    }
 
-    sort(items.begin(), items.end(), compare);
-    
-    int remaining_capacity = m;
-    double total_profit = 0.0;
+	for(int i = 0; i < number_of_items; i++){
+		cin >> items[i].first >> items[i].second;
+		ratio[i] = (double)items[i].first / items[i].second;
+		index[i] = i;
+	}
+	sort(index.begin(), index.end(), [&](int a, int b) {
+        return ratio[a] > ratio[b];
+    });
 
-    for (int i = 0; i < n; i++) {
-        if (items[i].weight <= remaining_capacity) {
-            remaining_capacity -= items[i].weight;
-            total_profit += items[i].value;
+	for (int i = 0; i < number_of_items && max_capacity > 0; i++) {
+        int idx = index[i];
+        int profit = items[idx].first;
+        int weight = items[idx].second;
+
+        if (weight <= max_capacity) {
+            max_capacity -= weight;
+            total_profit += profit;
         } else {
-            x[i] = (double)remaining_capacity / items[i].weight;
-            total_profit += items[i].value * x[i];
-            remaining_capacity = 0;
+            double fraction = (double)max_capacity / weight;
+            total_profit += profit * fraction;
+            max_capacity = 0;
         }
-
-        if (remaining_capacity == 0) break;
     }
 
     cout << "Maximum Profit: " << total_profit << endl;
 
-    return 0;
 }
+
+int32_t main(){
+    fast();
+    //int tc; cin >> tc;
+    //while(tc--){
+        thirteen();
+    //}
+}
+
+
+// input
+// 3 20
+// 25 18
+// 24 15
+// 15 10
