b2766555d060e8f7c8c7d6e620577fd5d48e1945
Debajyoti Das <debajyotid212@gmail.com>
2025-06-18 22:39:38 +0600

    Update TowerofHanoi.cpp

--- TowerofHanoi.cpp
+++ TowerofHanoi.cpp
@@ -1,23 +1,33 @@
+//Everything will be something that you've seen before, Don't panic.
+//For my Mother and Departed Father
+
 #include<bits/stdc++.h>
 #define fast() ios_base::sync_with_stdio(false);cin.tie(nullptr);
 #define ll long long
+#define nl '\n'
 using namespace std;
+const int N = 1e9;
 
-void ToH(int n, int s, int m, int d){
-	if(n == 1) {
-		cout << "S to D " << s << ' ' << d << '\n';
-		return;
-	}
-	ToH(n - 1, s, d, m);
-	cout << "S to D " << s << ' ' << d << '\n';
-	ToH(n - 1, m, s, d);
+void TowerOfHanoi(int n, char from_rod, char dest_rod, char aux_rod) {
+    if (n == 1) {
+        cout << "Move disk 1 from " << from_rod << " to " << dest_rod << endl;
+        return;
+    }
+    TowerOfHanoi(n - 1, from_rod, aux_rod, dest_rod);
+    cout << "Move disk " << n << " from " << from_rod << " to " << dest_rod << endl;
+    TowerOfHanoi(n - 1, aux_rod, dest_rod, from_rod);
+}
 
+
+void thirteen(){
+    int n; cin >> n;
+    TowerOfHanoi(n, 'A', 'B', 'C');
 }
 
 int32_t main(){
-  fast();
-  int n, s, m, d; cin >> n >> s >> m >> d;
-
-  ToH(n, s, d, m);
-  
+    fast();
+    //int tc; cin >> tc;
+    //while(tc--){
+        thirteen();
+    //}
 }
